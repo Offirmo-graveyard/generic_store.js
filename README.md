f13911305e1437e59c3f6762b170faaf1cb1e2e9
Offirmo <offirmo.net@gmail.com>
2014-01-06 13:02:35 +0100

    cleaned a few things. Hop Travis will pass at last !

--- README.md
+++ README.md
@@ -1,16 +1,30 @@
 generic_store.js
 ================
 
+[![Build Status](https://travis-ci.org/Offirmo/generic_store.js.png?branch=master)](https://travis-ci.org/Offirmo/generic_store.js)
+
 Introduction
 ------------
+
+generic_store.js wraps store.js and add :
+- an implementation in memory for safe tests or other use
+- an (optional) automatic key prefixing for avoiding conflicts
+
+NOTE : ONLY set() AND get() ARE IMPLEMENTED FOR NOW !
+
+User Story
+----------
 
 In you web app, you use the convenient local storage instead of fat cookies. Of course,
 you use store.js (https://github.com/marcuswestin/store.js) to access the local storage.
 
 Now there are two problems :
-- it's hard to unit test you app since local storage is persistent and shared
--
+- it's unsafe to unit test you app since local storage is persistent and shared
+- you may mess with another local storage user which use the same keys
 
+Solution :
+- Have a store.js implementation in memory for tests
+- Have an automatic prefixing of keys
 
 Usage
 -----
@@ -18,15 +32,23 @@ Usage
 ```javascript
 define(
 [
-	'network-constants/http'
+	'generic_store/generic_store',
 ],
-function(http_constants) {
+function(GenericStore) {
 	"use strict";
 
-	console.log( http_constants.methods.get ); // "GET"
-	console.log( http_constants.status_codes.status_400_client_error_bad_request ); // 400
-	console.log( http_constants.status_messages[404] ); // "Not Found"
-	...
+	// new store, in memory, guaranteed to be empty
+	var store1 = GenericStore.make_new("memory");
+	// new store, in memory, guaranteed to be empty, different from the previous one
+	var store2 = GenericStore.make_new("memory");
+	// existing store (local storage), "existing" reminds us that it may already contain data
+	var store3 = GenericStore.get_existing("local");
+
+	// now use your store like store.js
+	store.set('username', 'marcus');
+	store.get('username');
+	store.set('user', { name: 'marcus', likes: 'javascript' });
+
 ```
 
 
--- spec/common_store_api_spec.js
+++ spec/common_store_api_spec.js
@@ -16,83 +16,99 @@ function(chai) {
 	var should_implement_store_js_api = function() {
 		describe('store.js API implementation [shared]', function() {
 
-			it('should allow storage', function() {
-				var out = this.CUT.make_new();
+			describe("get() and set()", function() {
 
-				// just checking the API exists
-				// and doesn't trigger exceptions
-				out.set("foo", "bar");
-				out.set("toto", "titi");
-				out.set("toto", "tata"); // replace
-			});
+				it('should allow storage', function() {
+					var out = this.CUT.make_new();
 
-			it('should allow retrieving', function() {
-				var out = this.CUT.make_new();
+					// just checking the API exists
+					// and doesn't trigger exceptions
+					out.set("foo", "bar");
+					out.set("toto", "titi");
+					out.set("toto", "tata"); // replace
+				});
 
-				out.set("foo", "bar");
-				expect( out.get('foo') ).to.equals( 'bar' );
+				it('should allow retrieving', function() {
+					var out = this.CUT.make_new();
 
-				out.set("toto", "titi");
-				expect( out.get('toto') ).to.equals( 'titi' );
+					out.set("foo", "bar");
+					expect( out.get('foo') ).to.equals( 'bar' );
 
-				out.set("toto", "tata"); // replace
-				expect( out.get('toto') ).to.equals( 'tata' );
-			});
+					out.set("toto", "titi");
+					expect( out.get('toto') ).to.equals( 'titi' );
 
-			it('should be able to store objects but NOT store them by reference', function() {
-				var out = this.CUT.make_new();
+					out.set("toto", "tata"); // replace
+					expect( out.get('toto') ).to.equals( 'tata' );
+				});
 
-				var obj1 = { answer: 42 };
+			}); // describe feature
 
-				// simple set / get
-				out.set("obj1", obj1);
-				expect( out.get('obj1') ).to.deep.equals( { answer: 42 } );
+			it('should allow remove()');
 
-				// now directly change our local copy of obj
-				obj1.answer = 53;
+			it('should allow clear()');
 
-				// and check it didn't change in the store
-				expect( out.get('obj1') ).to.deep.equals( { answer: 42 } );
+			it('should allow getAll()');
 
-				// now level 2 : deep object
-				var obj2 = { version: 3, result: obj1 };
+			it('should allow forEach()');
 
-				out.set("obj2", obj2);
-				expect( out.get('obj2') ).to.deep.equals( { version: 3, result: { answer: 53 }} );
+			describe("underlying implementation", function() {
 
-				// modify again our local copy of obj
-				obj1.answer = 64;
-				// and check it didn't change in the store
-				expect( out.get('obj2') ).to.deep.equals( { version: 3, result: { answer: 53 }} );
-			});
+				it('should NOT store objects by reference', function() {
+					var out = this.CUT.make_new();
 
-			it('should be able to fetch objects but NOT serve them through a common reference', function() {
-				var out = this.CUT.make_new();
+					var obj1 = { answer: 42 };
 
-				var obj1 = { answer: 42 };
+					// simple set / get
+					out.set("obj1", obj1);
+					expect( out.get('obj1') ).to.deep.equals( { answer: 42 } );
 
-				// set it and retrieve it 2 times
-				out.set("obj1", obj1);
-				var get11 = out.get('obj1');
-				var get12 = out.get('obj1');
+					// now directly change our local copy of obj
+					obj1.answer = 53;
 
-				// now change one of the instance : the other should not change
-				get11.answer = 53;
-				expect( get12 ).to.deep.equals( { answer: 42 } );
+					// and check it didn't change in the store
+					expect( out.get('obj1') ).to.deep.equals( { answer: 42 } );
 
-				// now level 2 : deep object
-				var obj2 = { version: 3, result: obj1 };
+					// now level 2 : deep object
+					var obj2 = { version: 3, result: obj1 };
 
-				out.set("obj2", obj2);
-				var get21 = out.get('obj2');
-				var get22 = out.get('obj2');
+					out.set("obj2", obj2);
+					expect( out.get('obj2') ).to.deep.equals( { version: 3, result: { answer: 53 }} );
 
-				// now change one of the instance : the other should not change
-				get22.version = 4;
-				get22.result.answer = 64;
+					// modify again our local copy of obj
+					obj1.answer = 64;
+					// and check it didn't change in the store
+					expect( out.get('obj2') ).to.deep.equals( { version: 3, result: { answer: 53 }} );
+				});
 
-				expect( get21 ).to.deep.equals( { version: 3, result: { answer: 42 }} );
-			});
+				it('should NOT serve accessed objects through a common reference', function() {
+					var out = this.CUT.make_new();
+
+					var obj1 = { answer: 42 };
+
+					// set it and retrieve it 2 times
+					out.set("obj1", obj1);
+					var get11 = out.get('obj1');
+					var get12 = out.get('obj1');
+
+					// now change one of the instance : the other should not change
+					get11.answer = 53;
+					expect( get12 ).to.deep.equals( { answer: 42 } );
+
+					// now level 2 : deep object
+					var obj2 = { version: 3, result: obj1 };
+
+					out.set("obj2", obj2);
+					var get21 = out.get('obj2');
+					var get22 = out.get('obj2');
+
+					// now change one of the instance : the other should not change
+					get22.version = 4;
+					get22.result.answer = 64;
+
+					expect( get21 ).to.deep.equals( { version: 3, result: { answer: 42 }} );
+				});
+
+			}); // describe feature
 
 		}); // describe feature
 	};
